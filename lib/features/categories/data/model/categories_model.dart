5436df8a5f0a275d0f21480a8660be8f74abe4c3
amalhadila <amalhadila@gmail.com>
2024-03-20 21:05:16 +0200

    #models

--- lib/features/categories/data/model/categories_model.dart
+++ lib/features/categories/data/model/categories_model.dart
@@ -7,5 +7,17 @@ class Category {
     required this.imageCover,
   });
 
-  
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      name: json['name'],
+      imageCover: json['imageCover'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = name;
+    data['imageCover'] = imageCover;
+    return data;
+  }
 }
--- lib/features/categories/data/model/landmark_model.dart
+++ lib/features/categories/data/model/landmark_model.dart
@@ -27,6 +27,42 @@ class Landmark {
     required this.slug,
   });
 
+  factory Landmark.fromJson(Map<String, dynamic> json) {
+    return Landmark(
+      name: json['name'],
+      description: json['description'] ?? '',
+      imageCover: json['imageCover'],
+      images: List<String>.from(json['images']),
+      category: json['category'],
+      locationType: json['location']['type'],
+      coordinates: List<double>.from(json['location']['coordinates']),
+      governorate: json['location']['governorate'],
+      visitsNumber: json['visitsNumber'] ?? 0,
+      rating: json['rating'] ?? 4,
+      ratingsQuantity: json['ratingsQuantity'] ?? 0,
+      slug: json['slug'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['name'] = name;
+    data['description'] =description;
+    data['imageCover'] =imageCover;
+    data['images'] = images;
+    data['category'] = category;
+    data['location'] = {
+      'type': locationType,
+      'coordinates': coordinates,
+      'governorate': governorate,
+    };
+    data['visitsNumber'] = visitsNumber;
+    data['rating'] = rating;
+    data['ratingsQuantity'] = ratingsQuantity;
+    data['slug'] =slug;
+    return data;
+  }
+}
+
   
 
-}
